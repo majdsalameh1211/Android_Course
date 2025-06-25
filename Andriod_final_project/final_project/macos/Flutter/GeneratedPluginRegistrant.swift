c6ce8160e8bfd89484630ae87a9e46e20545b996
michael7billan <michael7billan@gmail.com>
2025-06-26 01:57:51 +0300

    add changes to yaml

--- Andriod_final_project/final_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Andriod_final_project/final_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
-import firebase_storage
+import firebase_database
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
